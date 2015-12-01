bdeffbf520c00dd5e457197b9858ad901f4ba3ff
Fengda HUANG <h@phodal.com>
2015-12-01 18:24:29 +0800

    download & remote copy

--- generators/app/index.js
+++ generators/app/index.js
@@ -32,8 +32,8 @@ module.exports = yeoman.generators.Base.extend({
       this.templatePath('dummyfile.txt'),
       this.destinationPath('dummyfile.txt')
     );
-    this.fetch("https://codeload.github.com/phodal-archive/echeveria-deploy/zip/master", "./", function(){
-      console.log("Get Builder Code...");
+    this.remote("https://codeload.github.com/phodal-archive/echeveria-deploy/zip/master", function(err, remote){
+      remote.copy('.', 'builder');
     })
   },
 
