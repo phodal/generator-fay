daf87885ece567a7a67769f67f6801e8e7abda68
Fengda HUANG <h@phodal.com>
2015-12-02 08:57:45 +0800

    try paht

--- generators/app/index.js
+++ generators/app/index.js
@@ -30,9 +30,9 @@ module.exports = yeoman.generators.Base.extend({
   writing: function () {
     this.fs.copy([
         this.templatePath() + 'fay-builder/**',
-        this.templatePath() + 'builder/**/.*',
+        this.templatePath() + 'fay-builder/**/.*',
         '!**/{gulpfile.js,bower.json,package.json,.git,.npmignore,.gitignore,wct.conf.js,docs,test}/**'],
-      this.destinationPath()
+      this.destinationPath('builder/')
     );
 
     this.fs.copy(this.templatePath('README.md'), this.destinationPath('README.md'));
